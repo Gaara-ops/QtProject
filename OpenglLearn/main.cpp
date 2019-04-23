3418767bccbe442f29fb8295e42ac7e65992ac4f
yuxing555 <2219502781@qq.com>
2019-04-23 17:59:56 +0800

    添加旋转变换

--- OpenglLearn/main.cpp
+++ OpenglLearn/main.cpp
@@ -43,8 +43,8 @@ vector向量的地址或者特殊的采用矩阵；
 
     // 4x4的平移变换矩阵
     Matrix4f World;
-    World.m[0][0] = 1.0f; World.m[0][1] = 0.0f; World.m[0][2] = 0.0f; World.m[0][3] = sinf(Scale);
-    World.m[1][0] = 0.0f; World.m[1][1] = 1.0f; World.m[1][2] = 0.0f; World.m[1][3] = 0.0f;
+    World.m[0][0] = cosf(Scale); World.m[0][1] =-sinf(Scale); World.m[0][2] = 0.0f; World.m[0][3] = 0.0f;
+    World.m[1][0] = sinf(Scale); World.m[1][1] = cosf(Scale); World.m[1][2] = 0.0f; World.m[1][3] = 0.0f;
     World.m[2][0] = 0.0f; World.m[2][1] = 0.0f; World.m[2][2] = 1.0f; World.m[2][3] = 0.0f;
     World.m[3][0] = 0.0f; World.m[3][1] = 0.0f; World.m[3][2] = 0.0f; World.m[3][3] = 1.0f;
     /**
